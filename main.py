32cb8120e18178be021b027b9898d4e6f299db85
Cencious <user.email>
2022-04-23 13:42:37 +0300

    add class user and its docstring

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import random
 """
 The users and credentials array are used to store all the users objecs and credentials
 """
+users=[]
+credentials=[]
+
+class User:
+    """
+    used to create an object containing username and password
+    """
