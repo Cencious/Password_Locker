dc6cc52e141dd3598905cc7ac09137d0bdbf32a8
Cencious <user.email>
2022-04-23 21:15:02 +0300

    define the function new. def new()

--- main.py
+++ main.py
@@ -25,4 +25,45 @@ class Credentials:
         print("\nSelect an option to continue")
         print("1.New credentials\n2.Existing credentials\n3.Vie credentials\n4.Delete credentials")
         option=input()
-    
+        if option=="1":
+            print("\nAdd the new contact to the account password manager: ")
+            site=input("Enter site name")
+            username=input("Enter username: ")
+            print("Do you want the application to generate a password for you?\n1.Yes\n2.No")
+        selection=input()
+        if selection=="1":
+            password=str(random.randit(1000000,9999999))
+        elif selection=="2":
+            password=input("Enter password: ")
+        else:
+            print("Invalid option")
+        credential=Credentials(site, username, password)
+        credentials.append(credential)
+        print("Added successfully")
+        Credentials.new()
+        if option=="2":
+            print("\nAdd the existing account to the password manager: ")
+            site=input("Enter site name: ")
+            username=input("Enter Username: ")
+            password=input("Enter password: ")
+            credential=Credentials(site, username, password)
+            credentials.append(credential)
+            print("Added successfully")
+            Credentials.new()
+        elif option=="3":
+            print("\nView accounts in password manager: ")
+            for x in credentials:
+                print("Site name: " + x.site)
+                print("username: " + x.username)
+                print("password: " + x.password)
+            Credentials.new() 
+        elif option=="4":
+            print("\nEnter the name of the site you want to delete")
+            name=input()
+            for i, o in enumerate(credentials):
+                if o.site==name:
+                    del credentials[i]
+            Credentials.new()
+        else: 
+            print("invalid option")
+
