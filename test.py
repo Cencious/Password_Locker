bad369f6c5f17f810909d77f69bc3ea7a8de1916
Cencious <user.email>
2022-04-24 16:11:51 +0300

    define the function test_credential

--- test.py
+++ test.py
@@ -10,5 +10,11 @@ class TestMain(unitest.TestCase):
     def test_user(self):
         self.assertEqual(self.user.username, "Cencious")
         self.assertEqual(self.user.password,"2022")
+    def test_credential(self):
+        self.assertEqual(self.cred.site,"facebook")
+        self.assertEqual(self.cred.username,"cencious")
+        self.assertEqual(self.cred.password,"1989")
+
+   
 
            
