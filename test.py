c41e5b528a4d74c505a6b8be8782ce3f1ce9166b
Cencious <user.email>
2022-04-24 16:07:58 +0300

    define the function setup

--- test.py
+++ test.py
@@ -2,3 +2,25 @@ import unittest
 from main import user,Credentials
 users=[]
 credentials=[]
+
+class TestMain(unitest.TestCase):
+    def setup(self):
+        self.user=User("Cencious", "2022")
+        self.cred=Credentials("facebook","dhonne","1989")
+    def test_user(self):
+        self.assertEqual(self.user.username, "Cencious")
+        self.assertEqual(self.user.password,"2022")
+
+    def test_credential(self):
+        self.assertEqual(self.cred.site,"facebook")
+        self.assertEqual(self.cred.username,"cencious")
+        self.assertEqual(self.cred.password,"1989")
+
+    def test_register(self):
+        user=User("cencious","2022")
+        users.append(user)
+        self.assertEqual(len(users),1)
+
+    if __name__=="__main__":
+        unitest.main()
+        
