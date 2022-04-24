7b49218466ea6be6cb7db02af04e09fa6706bc87
Cencious <user.email>
2022-04-24 15:53:24 +0300

    add navigate function

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import random
+import random 
 """
-The users and credentials array are used to store all the users objecs and credentials
+users and credentials array are used to store all the user objects and credential respectively
 """
-users=[]
+users=[] 
 credentials=[]
 
 class User:
@@ -15,36 +15,36 @@ class User:
 
 class Credentials:
     """
-    used to store site name, username and password.
-    """
-    def __init__(self, site, username, password):
+    used to store site name,username and password
+    """ 
+    def __init__(self, site ,username, password):
         self.site=site
         self.username=username
         self.password=password
     def new():
         print("\nSelect an option to continue")
-        print("1.New credentials\n2.Existing credentials\n3.Vie credentials\n4.Delete credentials")
+        print("1.New credentials\n2.Existing credentials\n3.View credentials\n4.Delete credentials")
         option=input()
         if option=="1":
-            print("\nAdd the new contact to the account password manager: ")
-            site=input("Enter site name")
-            username=input("Enter username: ")
-            print("Do you want the application to generate a password for you?\n1.Yes\n2.No")
-        selection=input()
-        if selection=="1":
-            password=str(random.randit(1000000,9999999))
-        elif selection=="2":
-            password=input("Enter password: ")
-        else:
-            print("Invalid option")
-        credential=Credentials(site, username, password)
-        credentials.append(credential)
-        print("Added successfully")
-        Credentials.new()
-        if option=="2":
-            print("\nAdd the existing account to the password manager: ")
+            print("\nAdd the new account to the password manager: ")
             site=input("Enter site name: ")
-            username=input("Enter Username: ")
+            username=input("Enter username: ")
+            print("Do you want the application to generate you a password?\n1.Yes\n2.No")
+            selection=input()
+            if selection=="1":
+                password=str(random.randint(1000000,9999999))
+            elif selection=="2":
+                password=input("Enter password: ")
+            else:
+                print("Invalid option")
+            credential=Credentials(site, username, password)
+            credentials.append(credential)
+            print("Added successfully")
+            Credentials.new()
+        elif option=="2":
+            print("\nAdd the existing account to the password manager:")
+            site=input("Enter site name: ")
+            username=input("Enter username: ")
             password=input("Enter password: ")
             credential=Credentials(site, username, password)
             credentials.append(credential)
@@ -56,21 +56,23 @@ class Credentials:
                 print("Site name: " + x.site)
                 print("username: " + x.username)
                 print("password: " + x.password)
-            Credentials.new() 
+            Credentials.new()
         elif option=="4":
-            print("\nEnter the name of the site you want to delete")
-            name=input()
+            
+            print("\nEnter the name of the site you want to delete: ")
+            name=input()            
             for i, o in enumerate(credentials):
-                if o.site==name:
+                if o.site== name:
                     del credentials[i]
             Credentials.new()
-        else: 
-            print("invalid option")
+        else:
+            print("Invalid option")
 
-class Main:
+class Main: 
     """
-    authenticate user on app
+    authenticating user on the app
     """
+    
     def register():
         print("\nCreate new account")
         username=input("Enter username: ")
@@ -82,7 +84,8 @@ class Main:
             Main.login()
         else:
             print("passwords do not match")
-            Main.register()         
+            Main.register() 
+        
             
     def login():
         print("\nLogin to your account")
@@ -113,4 +116,4 @@ print ("Welcome to Password Locker")
 print ("Select an option to continue")
 print ("1.Register\n2.Login\n3.Exit") 
 option=input()
-Main.navigate(option)      
+Main.navigate(option)
